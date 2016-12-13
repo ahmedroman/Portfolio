550e3cf8bbcd73f107b3ba02f997c34facf1fcc9
ahmedroman <mto1991@yahoo.com>
2016-12-13 21:19:56 +0600

    Add files via upload
    nnnn

---
+++ Updated-Portfolio - Copy/js/scripts.js
@@ -0,0 +1,18 @@
+// $(function(){
+
+
+	// /*Pricing Item Hover Effect*/
+	
+	// $('.pricing-item').hover(
+		// function(){
+			// $(this).addClass('active');
+		// },
+		// function(){
+			// $(this).removeClass('active');
+		// }
+	// );
+
+	// /*End Of Pricing Item Hover Effect*/
+	
+	
+// });
